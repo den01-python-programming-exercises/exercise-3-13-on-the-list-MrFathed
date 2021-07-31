2f8d02f3d3bf534874d519efb235e03485ab0332
Justin Turner <jmturner638@gmail.com>
2021-07-31 16:46:40 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #write your code below this line
+    names = []
+
+    while True:
+        name = input()
+
+        if name == '':
+            break
+
+        names.append(name)
+
+    query = input("Search for?")
+    if query in names:
+        print(query + " was found!")
+    else:
+        print(query + " was not found!")
 
 if __name__ == '__main__':
     main()
